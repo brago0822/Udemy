f5dcb44eb52733ae6a5df8d6416bd0956766196f
Brago0822 <brago0822@gmail.com>
2019-03-11 23:50:11 -0500

    Código de Sección 3 - Variables en Java

---
+++ UnversidadJava/src/Main.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+         /*-----------------------------------------------------------
+                                                    // Clase Variables
+        String holaMundo = "Hola Mundo";
+        System.out.println(holaMundo);
+        System.out.println(holaMundo);
+        System.out.println(holaMundo);
+
+        var saludo = "Hola";
+        System.out.println(saludo);*/
+
+        /*------------------------------------------------------------
+                                                    //Clase concatenación
+        var usuario = "Juan";
+        var saludar = "Hola";
+
+        System.out.println(saludar + " " + usuario);
+
+        //Saludos, mi nombre es Juan -> soutv
+        System.out.println("Saludos, mi nombre es " + usuario);*/
+
+        /* --------------------------------------------------------------
+                                                //Clase Caracteres Especiales
+        String nombre = "Karla";
+        String apellido = "Esparza";
+
+        System.out.println(nombre + " " + apellido);
+        System.out.println("Nueva Línea: \n" + nombre );
+        System.out.println("Tabulador: \t" + nombre );
+        System.out.println("Retroceso: \b" + nombre );
+        System.out.println("Retorno de carro: \r" + nombre );
+        System.out.println("Comilla Simple: \'" + nombre + "\'");
+        */
+
+        /* ------------------------------------------------------------------
+                                                                //Clase Scanner
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingresa tu nombre");
+        var usuario = scanner.nextLine();
+        var saludar = "Saludos";
+
+        System.out.println(saludar + " " + usuario);
+        */
+
+    }
+}
